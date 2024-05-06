07d6c7cd7128dfb4daa7cb7c01d1006c0429f5a0
ShyamPatel240704 <169038489+ShyamPatel240704@users.noreply.github.com>
2024-05-06 15:08:46 +0530

    Add files via upload

---
+++ Row Accending/RowAccending/RowAccending/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RowAccending
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[3, 3];
+            int temp = 0;
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.Write("a[{0},{1}] = ", row, col);
+                    a[row, col] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Befor Value ");
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.Write("{0}\t", a[row, col]);
+                }
+                Console.WriteLine();
+            }
+
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    for (int k = 1 + col; k < 3; k++)
+                    {
+                        if (a[row, col] > a[row, k])
+                        {
+                            temp = a[row, col];
+                            a[row, col] = a[row,k];
+                            a[row,k] = temp;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("After Value");
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.Write("{0}\t", a[row, col]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
