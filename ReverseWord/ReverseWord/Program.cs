a262149bd2f716c5c3990a0671ad94f651263d8f
ShyamPatel240704 <169038489+ShyamPatel240704@users.noreply.github.com>
2024-05-06 15:29:59 +0530

    Add files via upload

---
+++ ReverseWord/ReverseWord/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter The String: ");
+            string input = Console.ReadLine();
+
+            string[] words = input.Split(' ');
+            string[] reversedWords = new string[words.Length];
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                char[] charArray = words[i].ToCharArray();
+                Array.Reverse(charArray);
+                reversedWords[i] = new string(charArray);
+            }
+
+            string reversedString = string.Join(" ", reversedWords);
+            Console.WriteLine(reversedString);
+
+            Console.ReadKey();
+        }
+    }
+
+}
