a95b6064b03a1fdeffda378d05e6402ab88a750a
ShyamPatel240704 <169038489+ShyamPatel240704@users.noreply.github.com>
2024-05-06 15:31:38 +0530

    Add files via upload

---
+++ SupressCapital/SupressCapital/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupressCapital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            string UppeCase = "";
+            string LowerCase = "";
+            int Len;
+
+            Console.WriteLine("Please Enter Name = ");
+            Name = Console.ReadLine();
+
+            Len = Name.Length;
+
+
+            for (int i = 0; i < Len; i++)
+            {
+                char SuppChar = Name[i];
+
+                if (SuppChar >= 'A' && SuppChar <= 'Z')
+                {
+                    UppeCase += SuppChar;
+                }
+                else
+                {
+                    LowerCase += SuppChar;
+                }
+            }
+
+            Console.WriteLine("{0}{1}",LowerCase,UppeCase);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SupressCapital/WordCount/WordCount/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Name;
+            int Len,Wc = 1;
+
+            Console.WriteLine("Please Enter Your Name = ");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Name = {0}",Name);
+            Len = Name.Length;
+
+            Console.WriteLine("String Length = {0}",Len);
+
+            for (int i = 0; i < Len; i++)
+            {
+                if (Name[i] == ' ')
+                {
+                    Wc++;
+                }
+            }
+            Console.WriteLine("Word Count = {0}",Wc);
+
+            Console.ReadKey();
+        }
+    }
+}
