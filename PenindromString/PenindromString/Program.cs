8e41fe86fc5a29d6b69af9f753b0cad7fa7ac7ae
ShyamPatel240704 <169038489+ShyamPatel240704@users.noreply.github.com>
2024-05-06 15:27:59 +0530

    Add files via upload

---
+++ PenindromString/PenindromString/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PenindromString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            string Reverse = "";
+            int Len;
+
+            Console.Write("Please Ener Name = ");
+            Name = Console.ReadLine();
+
+
+            Len = Name.Length;
+
+            for (int i = Len - 1; i >= 0; i--)
+            {
+                Reverse += Name[i];
+            }
+
+            Console.WriteLine("{0}\t",Reverse);
+
+            if (Name == Reverse)
+            {
+                Console.WriteLine("Peindrom String");
+            }
+
+            else if (Name != Reverse)
+            {
+                Console.WriteLine("Not Pelindrom String");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
