7613414de155136ab790bea25e7e7f3882c032eb
ShyamPatel240704 <169038489+ShyamPatel240704@users.noreply.github.com>
2024-05-06 15:40:34 +0530

    Add files via upload

---
+++ RetunTypeFunctionDemo/RetunTypeFunctionDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetunFunctionDemo
+{
+    class Program
+    {
+        static int Sum(int a, int b)
+        {
+            int c = a + b;
+
+            return c;
+        }
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            Console.WriteLine("Please Enter 1st No = ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter 2nd No = ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            int Result = Sum(x, y);
+
+            Console.WriteLine("Total = {0}", Result);
+
+            Console.ReadKey();
+        }
+    }
+}
