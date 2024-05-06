a434cd4885f88ee630a6bdbb2fd6f04c29aefd15
ShyamPatel240704 <169038489+ShyamPatel240704@users.noreply.github.com>
2024-05-06 15:38:01 +0530

    Add files via upload

---
+++ VovelCount/VovelCount/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VovelCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            int Len,Count=0;
+
+            Console.Write("Please Enter Your Name = ");
+            Name = Console.ReadLine();
+
+
+            Console.WriteLine("Name = {0}",Name);
+
+            Len = Name.Length;
+
+            for (int i = 0; i < Len; i++)
+            {
+                if (Name[i] == 'A' || Name[i] == 'E' || Name[i] == 'I' || Name[i] == 'O' || Name[i] == 'U' || Name[i] == 'a' || Name[i] == 'e' || Name[i] == 'i' || Name[i] == 'o' || Name[i] == 'u')
+                {
+                    Count++;
+                }
+            }
+
+            Console.WriteLine("Vovel Count = {0}",Count);
+            Console.ReadKey();
+        }
+    }
+}
