1b80366099c7c6c025c2a00d45f08670ca60cf50
ShyamPatel240704 <169038489+ShyamPatel240704@users.noreply.github.com>
2024-05-06 14:53:17 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,68 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FullAseending
+namespace Matrix
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[,] a = new int[3, 3];
-            Console.WriteLine("Please Enter Values:- ");
-            for (int i = 0; i < 3; i++)
+            int [,] a = new int[3,3];
+            Console.WriteLine("Please Enter 3 x 3 Matrix Values:- ");
+            for (int row = 0; row < 3; row++)
             {
-                for (int j = 0; j < 3; j++)
+                for (int col = 0; col < 3; col++)
                 {
-                    Console.Write("a[{0},{1}]=", i, j);
-                    a[i, j] = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("a[{0} {1}]=",row,col);
+                    a[row,col]=Convert.ToInt32(Console.ReadLine());
                 }
             }
-            Console.WriteLine("Here 3x3 Matrix:- ");
-            for (int i = 0; i < 3; i++)
+            Console.WriteLine("Here 3 x 3 Matrix:- ");
+            for (int row = 0; row < 3; row++)
             {
-                for (int j = 0; j < 3; j++)
+                for (int col = 0; col < 3; col++)
                 {
-                    Console.Write("{0}\t",a[i,j]);
-                }
-                Console.WriteLine("");
-            }
-            Console.WriteLine("Full Assending :- ");
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    for (int k = 1+j; k < 3; k++)
-                    {
-                        if (a[i,j] > a[i,k])
-                        {
-                            int temp = a[i, j];
-                            a[i, j] = a[i, k];
-                            a[i, k] = temp;
-                        }                        
-                    }                                     
-                }
-            }
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    for (int p = 1 + i; p < 3; p++)
-                    {
-                        if (a[i, j] > a[p, j])
-                        {
-                            int temp = a[i, j];
-                            a[i, j] = a[p, j];
-                            a[p, j] = temp;
-                        }
-
-                    }  
-                }  
-            }
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.Write("{0}\t",a[i,j]);
+                    Console.Write("{0}\t",a[row,col]);
                 }
                 Console.WriteLine("");
             }
