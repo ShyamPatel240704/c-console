d2949c1993808d102806eaff9e4edcebfda1a847
ShyamPatel240704 <169038489+ShyamPatel240704@users.noreply.github.com>
2024-05-06 15:29:31 +0530

    Add files via upload

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            char[] CharPos;
+            int Len;
+
+            Console.WriteLine("Please Enter  Name = ");
+            Name = Console.ReadLine();
+
+            Len = Name.Length;
+
+            CharPos = new char[Len];
+
+            for (int i = Len - 1; i >= 0; i--)
+            {
+                Console.Write("{0}\t",Name[i] );
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
