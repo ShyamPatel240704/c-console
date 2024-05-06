afb15eaec60d6d25c3a525e8718d8846f0644cc7
ShyamPatel240704 <169038489+ShyamPatel240704@users.noreply.github.com>
2024-05-06 15:36:56 +0530

    Add files via upload

---
+++ ToggleCase/ToggleCase/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToggleCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            string UpperCase = "";
+            int Len;
+
+            Console.WriteLine("Please Enter Name = ");
+            Name = Console.ReadLine();
+
+            Len = Name.Length;
+
+            for (int i = 0; i < Len; i++)
+            {
+                char NewChar = Name[i];
+                if (NewChar >= 'A' && NewChar <= 'Z')
+                {
+                    UpperCase += Convert.ToChar(NewChar + 32);
+                }
+
+                else if (NewChar >= 'a' && NewChar <= 'z')
+                {
+                     UpperCase += Convert.ToChar(NewChar - 32);
+                }
+
+                else
+                {
+                    UpperCase += NewChar;
+                }
+            }
+
+            Console.WriteLine("{0}",UpperCase);
+
+            Console.ReadKey();
+        }
+    }
+}
