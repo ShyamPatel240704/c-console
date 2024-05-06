e672682e37665fc346a5cd775263a6961d183418
ShyamPatel240704 <169038489+ShyamPatel240704@users.noreply.github.com>
2024-05-06 15:26:21 +0530

    Add files via upload

---
+++ InsertString/InsertString/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            string NewChar = "";
+            int Pos = 0;
+            int Len;
+
+            Console.WriteLine("Please Enter Name = ");
+            Name = Console.ReadLine();
+
+            Len = Name.Length;
+
+            Console.WriteLine("Please Enter Neew Char = ");
+            NewChar = Console.ReadLine();
+
+            Console.WriteLine("Please Enter Pos No = ");
+            Pos = Convert.ToInt32(Console.ReadLine());
+
+            string ModiChar = "";
+            for (int i = 0; i < Len; i++)
+            {
+                if (i == Pos - 1)
+                {
+                    ModiChar += NewChar;
+                }
+                ModiChar += Name[i];
+            }
+            if (Pos == Len + 1)
+            {
+                ModiChar += NewChar;
+            }
+
+            Console.Write("{0}", ModiChar);
+
+            Console.ReadKey();
+        }
+    }
+}
