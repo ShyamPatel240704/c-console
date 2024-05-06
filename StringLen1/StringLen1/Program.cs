9810863c9814e5077a2202d52ae8b8081bbc0965
ShyamPatel240704 <169038489+ShyamPatel240704@users.noreply.github.com>
2024-05-06 15:30:59 +0530

    Add files via upload

---
+++ StringLen1/StringLen1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLen1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            int Len;
+
+            Console.Write("Please Enter Your Name = ");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Name = {0}",Name);
+            Len = Name.Length;
+           
+            Console.WriteLine("String Length = {0}",Len);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
