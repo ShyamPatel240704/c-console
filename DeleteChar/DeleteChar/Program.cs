c9e58218f5f2e192c34fa0ad0ace9a4b742bd0fa
ShyamPatel240704 <169038489+ShyamPatel240704@users.noreply.github.com>
2024-05-06 15:19:32 +0530

    Add files via upload

---
+++ DeleteChar/DeleteChar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            char DeleteChar;
+            int Len;
+
+            Console.WriteLine("Please Enter Name = ");
+            Name = Console.ReadLine();
+
+            Len = Name.Length;
+
+            Console.WriteLine("Please Enter Char = ");
+            DeleteChar = Convert.ToChar(Console.ReadLine());
+
+            for (int i = 0; i < Len; i++)
+            {
+                char PosChar = Name[i];
+                if (PosChar != DeleteChar)
+                {
+                    Console.Write("{0}", Name[i]);
+                }
+            }
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
