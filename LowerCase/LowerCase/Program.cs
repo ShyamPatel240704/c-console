61fdd58285c2aae3bc62a6e5f7261146b43690b7
ShyamPatel240704 <169038489+ShyamPatel240704@users.noreply.github.com>
2024-05-06 15:27:23 +0530

    Add files via upload

---
+++ LowerCase/LowerCase/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LowerCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            string LowerCase = "";
+            int Len;
+
+            Console.Write("Please Enter Name = ");
+            Name = Console.ReadLine();
+
+            Len = Name.Length;
+
+            for (int i = 0; i < Len; i++)
+            {
+                char NewChar = Name[i];
+                if (NewChar >= 'A' && NewChar <= 'Z')
+                {
+                    LowerCase += Convert.ToChar(NewChar + 32);                    
+                }
+                else
+                {
+                    LowerCase += NewChar;
+                }
+            }
+
+            Console.WriteLine("{0}",LowerCase);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
