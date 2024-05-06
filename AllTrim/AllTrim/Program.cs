934eebf8f598cc9a5285493384d91bd898449bba
ShyamPatel240704 <169038489+ShyamPatel240704@users.noreply.github.com>
2024-05-06 15:15:13 +0530

    Add files via upload

---
+++ AllTrim/AllTrim/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllTrim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            string UpperCase = "";
+            string Spc = "";
+            int Len;
+
+            Console.WriteLine("Please Enter Name = ");
+            Name = Console.ReadLine();
+
+            Len = Name.Length;
+
+            Console.WriteLine("Length = {0}", Len);
+
+            for (int i = 0; i < Len; i++)
+            {
+                char LeftChar = Name[i];
+
+                if (LeftChar >= 'A' && LeftChar <= 'Z' || LeftChar >= 'a' && LeftChar <= 'z')
+                {
+                    UpperCase += LeftChar;
+                }
+                else
+                {
+                    Spc += LeftChar;
+                }
+            }
+
+            Console.WriteLine("Modifird Name = {0}", UpperCase);
+
+
+            Console.ReadKey();
+        }
+    }
+}
