f6a3de6e4b5bbb49ab3f47d684f088f1ada67af9
ShyamPatel240704 <169038489+ShyamPatel240704@users.noreply.github.com>
2024-07-05 07:48:37 +0530

    Add files via upload

---
+++ RunTimeFileUpload/TaskOneFile/Default.aspx.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        FileUpload f1 = new FileUpload();
+        f1.ID = "fileupload1";
+        form1.Controls.Add(f1);
+
+        Button b1 = new Button();
+        b1.ID = "btn1";
+        b1.Text = "Click Here";
+        b1.Click += b1_Click;
+        form1.Controls.Add(b1);
+
+        form1.Controls.Add(new LiteralControl("<br><br><br>"));
+
+
+        Image m1 = new Image();
+        m1.ID = "Image1";
+        m1.Width = 500;
+        form1.Controls.Add(m1);
+
+        form1.Controls.Add(new LiteralControl("<br><br><br>"));
+
+
+        Button b2 = new Button();
+        b2.ID = "btn2";
+        b2.Text = "Delete";
+        b2.Click += b2_Click;
+        form1.Controls.Add(b2);
+
+        form1.Controls.Add(new LiteralControl("<br><br><br>"));
+
+        Label l1 = new Label();
+        l1.ID = "lbl1";
+        l1.Text = "Label1";
+        form1.Controls.Add(l1);
+
+        form1.Controls.Add(new LiteralControl("<br><br><br>"));
+
+        Label l2 = new Label();
+        l2.ID = "lbl2";
+        l2.Text = "Label2";
+        form1.Controls.Add(l2);
+
+        form1.Controls.Add(new LiteralControl("<br><br><br>"));
+
+        Label l3 = new Label();
+        l3.ID = "lbl3";
+        l3.Text = "Label3";
+        form1.Controls.Add(l3);
+    }
+
+    void b2_Click(object sender, EventArgs e)
+    {
+        Image img1 = FindControl("Image1") as Image;
+        Button bt1 = FindControl("btn2") as Button;
+        Label lb1 = FindControl("lbl1") as Label;
+        Label lb2 = FindControl("lbl2") as Label;
+        Label lb3 = FindControl("lbl3") as Label;
+
+        if (img1.ImageUrl != "")
+        {
+            File.Delete(Server.MapPath(img1.ImageUrl));
+            lb1.Text = "Name = ";
+            lb2.Text = "Size = ";
+            lb3.Text = "Ext = ";
+        }
+       
+    }
+
+    void b1_Click(object sender, EventArgs e)
+    {
+        FileUpload fl1 = FindControl("fileupload1") as FileUpload;
+        Image img1 = FindControl("Image1") as Image;
+        Label lb1 = FindControl("lbl1") as Label;
+        Label lb2 = FindControl("lbl2") as Label;
+        Label lb3 = FindControl("lbl3") as Label;
+
+
+        fl1.SaveAs(Server.MapPath("~/Image/" + fl1.FileName));
+        img1.ImageUrl = "~/Image/" + fl1.FileName;
+
+        lb1.Text = "Name = " +  fl1.FileName;
+
+        Int64 Size = fl1.FileContent.Length / 1024;
+        lb2.Text = "Size = " + Size + "kb";
+
+        string Ext = Path.GetExtension(fl1.FileName);
+        lb3.Text = "Ext = " + Ext;
+
+    }
+}
---
+++ RunTimeFileUpload/TaskTwoLoop/Default.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        FileUpload f1 = new FileUpload();
+        f1.ID = "FileUpload1";
+        form1.Controls.Add(f1);
+
+        Button b1 = new Button();
+        b1.ID = "btn1";
+        b1.Text = "Click Here";
+        b1.Click += b1_Click;
+        form1.Controls.Add(b1);
+
+        form1.Controls.Add(new LiteralControl("&nbsp;&nbsp;&nbsp;"));
+
+        Button b2 = new Button();
+        b2.ID = "btn2";
+        b2.Text = "ViewImage";
+        b2.Click += b2_Click;
+        form1.Controls.Add(b2);
+
+        form1.Controls.Add(new LiteralControl("<br><br><br>"));
+
+        Image image1 = new Image();
+        image1.ID = "img1";
+        image1.Width = 500;
+        form1.Controls.Add(image1);
+
+        form1.Controls.Add(new LiteralControl("<br><br><br>"));
+
+        Label l1 = new Label();
+        l1.ID = "lbl1";
+        l1.Text = "";
+        form1.Controls.Add(l1);
+
+       
+
+    }
+    static int Count = 1;
+    void b2_Click(object sender, EventArgs e)
+    {
+
+        for (int i = 1; i < Count; i++)
+        {
+            Image i1 = new Image();
+            i1.ID = "img" + i;
+            i1.Width = 500;
+            i1.Height = 500;
+            i1.ImageUrl = Session["s" + i].ToString();
+
+            form1.Controls.Add(i1);
+        }
+
+    }
+
+  
+
+    void b1_Click(object sender, EventArgs e)
+    {
+        FileUpload fl1 = FindControl("FileUpload1") as FileUpload;
+        Image im1 = FindControl("img1") as Image;
+        Label lb1 = FindControl("lbl1") as Label;
+
+        fl1.SaveAs(Server.MapPath("~/Image/" + fl1.FileName));
+        im1.ImageUrl = "~/Image/" + fl1.FileName;
+
+        Session["s" + Count] = "~/Image/" + fl1.FileName;
+
+        Int64 Size = fl1.FileContent.Length / 1024;
+        string Ext = Path.GetExtension(fl1.FileName);
+
+        lb1.Text += Count + " " + "Name = " + fl1.FileName + " " + " ( Size = " + Size + " kb) " + "(Ext = " + Ext + ")" + "<br>";
+
+
+        Count++;
+
+
+
+
+
+    }
+
+    
+
+}
