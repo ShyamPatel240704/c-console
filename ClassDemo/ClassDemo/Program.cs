e4306227ba592f3cb7e2f435994e3c9bdcba4bd2
ShyamPatel240704 <169038489+ShyamPatel240704@users.noreply.github.com>
2024-07-05 07:56:58 +0530

    Add files via upload

---
+++ ClassDemo/ClassDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student x = new Student();
+
+            x.RollNo = 120;
+            x.Name = "Shyam";
+
+            Console.Write("RollNo \t Name");
+            Console.WriteLine();
+            Console.Write("{0} \t {1}",x.RollNo,x.Name);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClassDemoInput/ClassDemoInput/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student Y = new Student();
+
+            Console.WriteLine("Please Enter No");
+
+
+            Console.ReadKey();
+        }
+    }
+}
