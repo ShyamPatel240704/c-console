51431f4753238609bdb1a15d8a0a2b13b54605bb
ShyamPatel240704 <169038489+ShyamPatel240704@users.noreply.github.com>
2024-07-12 07:35:49 +0530

    Add files via upload

---
+++ Pett5/Pett5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pett5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Name;
+            int Len;
+
+            Console.WriteLine("Please Enter Name = ");
+            Name = Console.ReadLine();
+
+            Len = Name.Length;
+
+            //for (int row = 0; row < Len; row++)
+            //{
+            //    for (int spc = 0; spc < row; spc++)
+            //    {
+            //        Console.Write("\t");
+            //    }
+            //    for (int col = 0; col < Len; col++)
+            //    {
+            //        Console.Write("{0}\t",Name[col]);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            for (int row = 0; row < Len; row++)
+            {
+                for (int spc = 0; spc < row; spc++)
+                {
+                    Console.Write("\t");
+                }
+                for (int col = (Len-1) - row; col >=0; col--)
+                {
+                    Console.Write("{0}\t",  Name[col]);
+                }
+                Console.WriteLine();
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
