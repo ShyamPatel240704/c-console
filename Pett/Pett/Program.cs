3bdf0d3a73b2fddf191554e4496c7695eff58ef4
ShyamPatel240704 <169038489+ShyamPatel240704@users.noreply.github.com>
2024-07-12 07:33:54 +0530

    Add files via upload

---
+++ Pett/Pett/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pett
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            int Len;
+
+            Console.WriteLine("Please Enter Name = ");
+            Name = Console.ReadLine();
+
+            Len = Name.Length;
+
+            for (int Row = 0; Row <= Len; Row++)
+            {
+                for (int Col = 0; Col < Row; Col++)
+                {
+                    Console.Write("{0}\t",Name[Col]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
